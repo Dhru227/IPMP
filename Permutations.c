d101b9ef053e8e67027d75618a7d2715c1fa0921
Dru <88442075+Dhru227@users.noreply.github.com>
2023-02-19 12:24:48 +0530

    Add files via upload

---
+++ Permutations.c
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void permute(string s, int l, int r)
+{if (l == r)
+     cout<<s<<" ";
+   else{
+        for (int i = l; i <= r; i++)
+      {swap(s[l], s[i]);
+      permute(s, l+1, r);
+      swap(s[l], s[i]);
+      }
+      }
+      }
+int main()
+{
+     string str = "EFG";
+     int n = str.size();
+     permute(str, 0, n-1);
+     return 0;
+}
